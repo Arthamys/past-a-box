5f2f332e1597a0be24456d1a4c55c754aab130ff
Galilee 'Bill' Enguehard <galilee.enguehard@gmail.com>
2019-02-13 12:15:14 +0100

    Fix priority and default value for config

--- api/src/common/config.rs
+++ api/src/common/config.rs
@@ -1,21 +1,12 @@
 use std::result;
 
 /// Configuration fields for an API client
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     /// The Unix Domain Socket on which to connect to establish communication
     /// with the API server.
+    /// Defaults to `/tmp/pasta.sock`
     pub socket: String,
-}
-
-/// Default values for the client configuration.
-/// socket: '/var/run/pasta.sock'
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            socket: String::from("/var/run/pasta.sock"),
-        }
-    }
 }
 
 impl Config {
@@ -26,14 +17,19 @@ impl Config {
     /// Configuration can be overriden through environment variables.
     /// The env variables have to start with `PAB_` and the field to override.
     pub fn parse() -> result::Result<Config, config::ConfigError> {
-        let mut cfg = config::Config::try_from(&Config::default()).unwrap();
+        let mut cfg = config::Config::default();
+        cfg.set_default("socket", "/tmp/pasta.sock").unwrap();
         match cfg.merge(config::File::with_name("config.yaml")) {
-            Ok(_) => {}
-            Err(_) => info!("No configuration file client."),
+            Ok(_) => (),
+            Err(e) => {
+                info!("gathering configuration file: {:?}", e);
+            }
         };
-        match cfg.merge(config::Environment::with_prefix("PAB_")) {
-            Ok(_) => {}
-            Err(_) => info!("No environment variable overrides."),
+        match cfg.merge(config::Environment::with_prefix("PAB")) {
+            Ok(_) => (),
+            Err(e) => {
+                info!("gathering environment variables: {:?}", e);
+            }
         };
         cfg.try_into()
     }
