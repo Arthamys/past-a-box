52b9a4d9b17dfb65baf5d531c71d778975882041
Galilee 'Bill' Enguehard <galilee.enguehard@gmail.com>
2019-02-19 13:59:51 +0100

    Remove warning from cli binary

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,8 +2,8 @@ use api::client::Client;
 
 fn main() {
     env_logger::init();
-    let mut api_client = Client::new();
+    let api_client = Client::new();
     println!("api client: {:?}", api_client);
-    let test_clipping = String::from("test_clipping");
+    let _test_clipping = String::from("test_clipping");
     api_client.unwrap().request_clipping();
 }
