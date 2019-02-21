8696b0cc375b88a7c6127d245e0e4e673b43926d
Galilee 'Bill' Enguehard <galilee.enguehard@gmail.com>
2019-02-21 12:55:56 +0100

    Use clipboard module daemon

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,11 +1,14 @@
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate lazy_static;
 extern crate api;
 extern crate env_logger;
 extern crate libc;
 extern crate wayland_client;
 extern crate wayland_protocols;
 
+mod clipboard;
 mod error;
 mod wayland;
 
@@ -15,7 +18,10 @@ use api::server::Response;
 use api::server::Server;
 use std::sync::{Arc, Mutex};
 use std::thread;
-use wayland::WaylandContext;
+
+lazy_static! {
+    static ref DAEMON: Mutex<Daemon> = Mutex::new(Daemon::new());
+}
 
 struct Daemon {
     storage: Arc<Mutex<Vec<Clipping>>>,
@@ -23,7 +29,7 @@ struct Daemon {
 
 impl Daemon {
     /// Create a new Daemon instance
-    fn new() -> Daemon {
+    pub fn new() -> Daemon {
         Daemon {
             storage: Arc::new(Mutex::new(Vec::new())),
         }
@@ -43,25 +49,19 @@ fn main() {
     let api_hdl = thread::Builder::new()
         .name("api_listener".into())
         .spawn(move || {
-            let srv = Server::new(Box::new(handler)).unwrap();
+            let srv = Server::new(Box::new(api_handler)).unwrap();
             srv.run();
         })
         .expect("Could not spawn api server thread");
 
-    let clipboard_hdl = thread::Builder::new()
-        .name("clipboard listener".into())
-        .spawn(|| {
-            let mut ctx = WaylandContext::new().unwrap();
-            ctx.register_handler(|_offer, _mime_type| info!("In the handler"))
-        })
-        .expect("Could not spawn clipboard listener thread");
+    let clipboard_hdl = clipboard::Clipboard::new_clipboard_thread();
 
     api_hdl.join().expect("api_server_thread crashed");
     clipboard_hdl.join().expect("clipboard_thread crashed");
 }
 
 // the handler will need to have access to the daemon storage
-fn handler(_rq: Request) -> Response {
+fn api_handler(_rq: Request) -> Response {
     let rsp = Vec::new();
     Response::Clippings(rsp)
 }
